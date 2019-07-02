be9f3a5ff3f1bb3e47c0068c487944ef68dceb44
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-07-01 17:21:10 -0700

    add functionality to guessing wrong. Removes from choices and lowers life.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
 //GLOBAL VARIABLES
-var guesses, wins, losses, remainder, wordList, answer;
+var guesses, wins, losses, remainder, wordList, answer, alphabetArr;
+alphabetArr = ("abcdefghijklmnopqrstuvwxyz").split("");
 wordList = ["ANAKIN", "DARTH VADER", "LUKE", "OBI-WAN", "C-3PO", "R2-D2", "CHEWBACCA", "HAN", "LEIA", "PADME", "QUI-GON", "EMPEROR", "YODA", "JAR JAR", "LANDO", "RED LEADER", "WEDGE", "BAIL ORGANA", "CAPTAIN PANAKA", "MACE WINDU", "NUTE GUNRAY", "BIGGS", "GOLD LEADER", "GENERAL GRIEVOUS", "TARKIN", "PIETT", "FODE/BEED", "RIC OLIE", "SHMI", "RUNE", "BOBA FETT", "OWEN", "ADMIRAL ACKBAR", "WATTO", "LAMA SU", "COUNT DOOKU", "GOLD FIVE", "RED TEN", "SIO BIBBLE", "JABBA", "MAD MADDA", "TAUN WE", "RIEEKAN", "CAPTAIN TYPHO", "ZEV", "DARTH MAUL", "BRAVO TWO", "JANGO FETT", "BOSS NASS", "GENERAL CEEL", "TEY HOW", "TC-14", "KITSTER", "RUWEE", "CLONE COMMANDER CODY", "CLONE COMMANDER GREE", "KI-ADI-MUNDI", "ODD BALL", "MON MOTHMA", "BERU", "DACK", "JANSON", "SEBULBA", "JIRA", "VALORUM", "WALD", "BRAVO THREE", "DOFINE", "TARPALS", "PK-4", "GREEDO", "CLIEGG", "SENATOR ASK AAK", "JOBAL", "ORN FREE TAA", "POGGLE", "SOLA", "SUN RIT", "PLO KOON", "FANG ZAR", "TION MEDON", "ON MEDON", "CAPTAIN ANTILLES", "GIDDEAN DANU", "MOTTI", "CAMIE", "DODONNA", "DERLIN", "OZZEL", "NEEDA", "BIB FORTUNA", "JERJERROD", "BOUSHH", "RABE", "REY", "POE", "SNAP", "GENERAL HUX", "SNOKE", "COLONEL DATOO", "KYLO REN", "JESS", "ELLO ASTY", "NIV LEK", "YOLO ZIFF", "FINN", "ADMIRAL STATURA", "CAPTAIN PHASMA", "MAZ", "BALA-TIK", "LIEUTENANT MITAKA", "UNKAR PLUTT", "LOR SAN TEKKA", "BB-8"];
-answer = wordList[Math.floor(Math.random() * Math.floor(wordList.length))];
+answer = wordList[Math.floor(Math.random() * Math.floor(wordList.length))].toLowerCase();
 answerArr = Array.from(answer);
 console.log(answer.toLocaleString());
+console.log("==========================");
+console.log(alphabetArr.toLocaleString());
 guesses = [];
 wins = 0;
 losses = 0;
@@ -23,8 +26,16 @@ document.onkeyup = function (event) {
 
     // Captures the key press, converts it to lowercase, and saves it to a variable.
     var letter = event.key.toLowerCase();
-    if (answer.indexOf(letter) === -1) {
+    if (alphabetArr.indexOf(letter) === -1){
+        alert("Please only press alphabet characters.");
+    } else if (answer.indexOf(letter) === -1) {
+        remainder--;
         guesses.push(letter);
+        alphabetArr.splice(alphabetArr.indexOf(letter),1);
+        console.log(alphabetArr.toLocaleString());
+    }else{
+        guesses.push(letter);
+        // answerArr.splice()
     }
 
     updateStats();
