d4eba0116817e8134d4567714e9600f557f46234
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-07-02 12:20:19 -0700

    messing around with how to output underscores. Didn't work out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //GLOBAL VARIABLES
-var guesses, wins, losses, remainder, wordList, answer, alphabetArr;
+var guesses, wins, losses, remainder, wordList, answer, alphabetArr, underscoreArr, letterArr;
 alphabetArr = ("abcdefghijklmnopqrstuvwxyz").split("");
 wordList = ["ANAKIN", "DARTH VADER", "LUKE", "OBI-WAN", "C-3PO", "R2-D2", "CHEWBACCA", "HAN", "LEIA", "PADME", "QUI-GON", "EMPEROR", "YODA", "JAR JAR", "LANDO", "RED LEADER", "WEDGE", "BAIL ORGANA", "CAPTAIN PANAKA", "MACE WINDU", "NUTE GUNRAY", "BIGGS", "GOLD LEADER", "GENERAL GRIEVOUS", "TARKIN", "PIETT", "FODE/BEED", "RIC OLIE", "SHMI", "RUNE", "BOBA FETT", "OWEN", "ADMIRAL ACKBAR", "WATTO", "LAMA SU", "COUNT DOOKU", "GOLD FIVE", "RED TEN", "SIO BIBBLE", "JABBA", "MAD MADDA", "TAUN WE", "RIEEKAN", "CAPTAIN TYPHO", "ZEV", "DARTH MAUL", "BRAVO TWO", "JANGO FETT", "BOSS NASS", "GENERAL CEEL", "TEY HOW", "TC-14", "KITSTER", "RUWEE", "CLONE COMMANDER CODY", "CLONE COMMANDER GREE", "KI-ADI-MUNDI", "ODD BALL", "MON MOTHMA", "BERU", "DACK", "JANSON", "SEBULBA", "JIRA", "VALORUM", "WALD", "BRAVO THREE", "DOFINE", "TARPALS", "PK-4", "GREEDO", "CLIEGG", "SENATOR ASK AAK", "JOBAL", "ORN FREE TAA", "POGGLE", "SOLA", "SUN RIT", "PLO KOON", "FANG ZAR", "TION MEDON", "ON MEDON", "CAPTAIN ANTILLES", "GIDDEAN DANU", "MOTTI", "CAMIE", "DODONNA", "DERLIN", "OZZEL", "NEEDA", "BIB FORTUNA", "JERJERROD", "BOUSHH", "RABE", "REY", "POE", "SNAP", "GENERAL HUX", "SNOKE", "COLONEL DATOO", "KYLO REN", "JESS", "ELLO ASTY", "NIV LEK", "YOLO ZIFF", "FINN", "ADMIRAL STATURA", "CAPTAIN PHASMA", "MAZ", "BALA-TIK", "LIEUTENANT MITAKA", "UNKAR PLUTT", "LOR SAN TEKKA", "BB-8"];
 answer = wordList[Math.floor(Math.random() * Math.floor(wordList.length))].toLowerCase();
@@ -8,18 +8,23 @@ console.log(answer.toLocaleString());
 console.log("==========================");
 console.log(alphabetArr.toLocaleString());
 guesses = [];
+underscoreArr = [];
+letterArr = [];
 wins = 0;
 losses = 0;
 remainder = 9;
 
 //HTML Variables
-var hWins, hLosses, hRemainder, hGuesses, hAnswer;
+var hWins, hLosses, hRemainder, hGuesses, hAnswer, hWordArea;
 hWins = document.getElementById("wins");
 hLosses = document.getElementById("losses");
 hRemainder = document.getElementById("remainder");
 hGuesses = document.getElementById("guesses");
 hAnswer = document.getElementById("answer");
-
+hWordArea = document.getElementById("wordArea");
+generateHTML();
+// g = document.createElement('div');
+// g.setAttribute("id", "Div1");
 
 // Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
 document.onkeyup = function (event) {
@@ -47,4 +52,22 @@ function updateStats() {
     hRemainder.innerHTML = "Guesses Left: " + remainder;
     hGuesses.innerHTML = "Your guesses thus far: " + guesses.toLocaleString();
     hAnswer.innerHTML = answer;
+}
+
+function generateHTML(){
+    var underscore = document.createElement('span');
+    underscore.setAttribute("id", "underscore");
+    underscore.innerHTML = "_";
+    var spacebar = document.createElement('span');
+    spacebar.setAttribute("id","spacebar");
+    spacebar.innerHTML = " ";
+    for(var i = 0; i < answerArr.length; i++){
+        if(alphabetArr.indexOf(answerArr[i]) === -1){
+            console.log("adding space bar");
+            hWordArea.appendChild(spacebar);
+        }else{
+            console.log("adding underscore");
+            hWordArea.appendChild(underscore);
+        }
+    }
 }
