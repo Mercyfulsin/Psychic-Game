61b0b7b6f4002e34354f4fd9b935fbd14b014f47
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-07-06 02:14:35 -0700

    Toast refreshes page on close

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //GLOBAL VARIABLES
 var guesses, wins, losses, remainder, wordList, answer, alphabetArr, htmlArr, answerRemainder;
-alphabetArr = ("abcdefghijklmnopqrstuvwxyz").split("");
+alphabetArr = ("abcdefghijklmnopqrstuvwxyz1234567890").split("");
 wordList = ["ANAKIN", "DARTH VADER", "LUKE", "OBI-WAN", "C-3PO", "R2-D2", "CHEWBACCA", "HAN", "LEIA", "PADME", "QUI-GON", "EMPEROR", "YODA", "JAR JAR", "LANDO", "RED LEADER", "WEDGE", "BAIL ORGANA", "CAPTAIN PANAKA", "MACE WINDU", "NUTE GUNRAY", "BIGGS", "GOLD LEADER", "GENERAL GRIEVOUS", "TARKIN", "PIETT", "FODE/BEED", "RIC OLIE", "SHMI", "RUNE", "BOBA FETT", "OWEN", "ADMIRAL ACKBAR", "WATTO", "LAMA SU", "COUNT DOOKU", "GOLD FIVE", "RED TEN", "SIO BIBBLE", "JABBA", "MAD MADDA", "TAUN WE", "RIEEKAN", "CAPTAIN TYPHO", "ZEV", "DARTH MAUL", "BRAVO TWO", "JANGO FETT", "BOSS NASS", "GENERAL CEEL", "TEY HOW", "TC-14", "KITSTER", "RUWEE", "CLONE COMMANDER CODY", "CLONE COMMANDER GREE", "KI-ADI-MUNDI", "ODD BALL", "MON MOTHMA", "BERU", "DACK", "JANSON", "SEBULBA", "JIRA", "VALORUM", "WALD", "BRAVO THREE", "DOFINE", "TARPALS", "PK-4", "GREEDO", "CLIEGG", "SENATOR ASK AAK", "JOBAL", "ORN FREE TAA", "POGGLE", "SOLA", "SUN RIT", "PLO KOON", "FANG ZAR", "TION MEDON", "ON MEDON", "CAPTAIN ANTILLES", "GIDDEAN DANU", "MOTTI", "CAMIE", "DODONNA", "DERLIN", "OZZEL", "NEEDA", "BIB FORTUNA", "JERJERROD", "BOUSHH", "RABE", "REY", "POE", "SNAP", "GENERAL HUX", "SNOKE", "COLONEL DATOO", "KYLO REN", "JESS", "ELLO ASTY", "NIV LEK", "YOLO ZIFF", "FINN", "ADMIRAL STATURA", "CAPTAIN PHASMA", "MAZ", "BALA-TIK", "LIEUTENANT MITAKA", "UNKAR PLUTT", "LOR SAN TEKKA", "BB-8"];
 answer = wordList[Math.floor(Math.random() * Math.floor(wordList.length))].toLowerCase();
 answerArr = Array.from(answer);
@@ -11,7 +11,7 @@ guesses = [];
 htmlArr = [];
 answerRemainder = answerArr.slice();
 answerRemainder.forEach(function(content,index,obj){
-    if(content === " "){obj.splice(index,1);}
+    if(alphabetArr.indexOf(content) === -1){obj.splice(index,1);}
 });
 wins = 0;
 losses = 0;
@@ -19,16 +19,28 @@ remainder = 9;
 
 //HTML Variables
 var hWins, hLosses, hRemainder, hGuesses, hAnswer, hWordArea, hUnderscoreArea,toastText;
-hWins = document.getElementById("wins");
-hLosses = document.getElementById("losses");
+// hWins = document.getElementById("wins");
+// hLosses = document.getElementById("losses");
 hRemainder = document.getElementById("remainder");
 hGuesses = document.getElementById("guesses");
-hAnswer = document.getElementById("answer");
+// hAnswer = document.getElementById("answer");
 hWordArea = document.getElementById("wordArea");
 hUnderscoreArea = document.getElementById("underscoreArea");
 toastText = document.getElementsByClassName("toast-body");
 generateHTML();
 
+$(document).ready(function(){
+    $(".toast").toast();
+    $("#myToast").on('hidden.bs.toast', function(){
+        window.location.reload();
+    });
+});
+
+
+
+$("#myToast").on('show.bs.toast', function(){
+    console.log("testingSHOW");
+});
 // Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
 document.onkeyup = function (event) {
     // Captures the key press, converts it to lowercase, and saves it to a variable.
@@ -61,11 +73,11 @@ function showCharacter(letter) {
 
 
 function updateStats() {
-    hWins.innerHTML = "Wins: " + this.wins;
-    hLosses.innerHTML = "Losses: " + losses;
+    // hWins.innerHTML = "Wins: " + this.wins;
+    // hLosses.innerHTML = "Losses: " + losses;
     hRemainder.innerHTML = "Guesses Left: " + remainder;
     hGuesses.innerHTML = "Your guesses thus far: " + guesses.toLocaleString();
-    hAnswer.innerHTML = answer;
+    // hAnswer.innerHTML = answer;
     if(answerRemainder.length === 0){
         alphabetArr = [];
         toastText[0].innerHTML = "You won!";
